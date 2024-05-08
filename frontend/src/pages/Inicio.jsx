73d83359b629470545ebbbd4e0b77b5c09943d08
BenjaminVeli <benjamin.veli@tecsup.edu.pe>
2024-05-07 22:31:45 -0500

    Estilos de inicio

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,9 +1,24 @@
 import Header from "../components/Header"
+import { Link } from "react-router-dom";
+
+
+import "../styles/inicio.css";
 
 function Inicio() {
     return <div>
         <Header />
         
+        <section id="entrada-principal" className="section--entrada--principal">
+            <div className="container">
+                    <h1 className="section--entrada__h1">Bienvenido a Exloratec</h1>
+                    <h5 className="section--entrada__h5">Tour 360° del campus Lima</h5>
+                    <div data-aos="fade-up">
+                    <Link to="/login" className="btn btn-begin ">Empezar</Link>
+                    </div>
+            </div>
+        </section>
+        
+
     </div>
 }
 
